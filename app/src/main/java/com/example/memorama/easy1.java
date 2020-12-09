2d5accd50bcc4c22e742568449c336288e2be159
Fatih Gulen <fatih.gulen@outlook.com>
2020-12-09 13:10:31 +0100

    first xml files

---
+++ app/src/main/java/com/example/memorama/easy1.java
@@ -0,0 +1,14 @@
+package com.example.memorama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class easy1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy1);
+    }
+}
